74f9009e3a3d914ec2cbeb08bcf7e31ba9ff36b8
Brendan Cazier <admin@brendancazier.com>
2022-09-02 23:05:27 +0000

    Day 2

--- golang/internal/year_2021/day_02/day_02.go
+++ golang/internal/year_2021/day_02/day_02.go
@@ -1,1 +1,73 @@
-package day_02
+package main
+
+import (
+	"fmt"
+	helpers "main/pkg/helpers"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func PartOne() int64 {
+	cwd, _ := os.Getwd()
+	var data string = helpers.LoadInput(cwd, 2021, 02)
+	var commands []string = helpers.SplitLine(data)
+
+	var x int64 = 0
+	var y int64 = 0
+
+	for i := 0; i < len(commands); i++ {
+		splits := strings.SplitN(commands[i], " ", 2)
+
+		direction := splits[0]
+		amount, _ := strconv.ParseInt(splits[1], 10, 64)
+
+		switch direction {
+		case "forward":
+			x += amount
+		case "up":
+			y -= amount
+		case "down":
+			y += amount
+		default:
+			panic("Something went wrong sorry")
+		}
+	}
+	return x * y
+}
+
+func PartTwo() int64 {
+	cwd, _ := os.Getwd()
+	var data string = helpers.LoadInput(cwd, 2021, 02)
+	var commands []string = helpers.SplitLine(data)
+
+	var x int64 = 0
+	var y int64 = 0
+	var aim int64 = 0
+
+	for i := 0; i < len(commands); i++ {
+		splits := strings.SplitN(commands[i], " ", 2)
+
+		direction := splits[0]
+		amount, _ := strconv.ParseInt(splits[1], 10, 64)
+
+		switch direction {
+		case "forward":
+			x += amount
+			y += aim * amount
+		case "up":
+			aim -= amount
+		case "down":
+			aim += amount
+		default:
+			panic("Something went wrong sorry")
+		}
+		fmt.Print()
+	}
+	return x * y
+}
+
+func main() {
+	fmt.Println("Part One:", PartOne())
+	fmt.Println("Part Two:", PartTwo())
+}
