5bd439cd06515e32b2467e1e31c59e4c95043b6c
Brendan Cazier <admin@brendancazier.com>
2022-09-26 01:37:59 +0000

    Day 3!

---
+++ golang/2021/03/three_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPartOne(t *testing.T) {
+	var expected int = 198
+	var output int = PartOne(sample_input)
+
+	assert.Equal(t, output, expected)
+}
+func TestPartTwo(t *testing.T) {
+	var expected int = 230
+	var output int = PartTwo(sample_input)
+
+	assert.Equal(t, output, expected)
+}
