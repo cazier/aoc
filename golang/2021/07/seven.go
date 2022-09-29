6a4b54218db23f71c4371d0a094e7751921cac5e
Brendan Cazier <admin@brendancazier.com>
2022-09-29 21:13:48 +0000

    Day 7

---
+++ golang/2021/07/seven.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	utils "main/utils"
+	"main/utils/splits"
+	"math"
+)
+
+const sample_input string = `
+16,1,2,0,4,2,7,1,2,14
+`
+
+func PartOne(input string) int {
+	crabs := utils.StringToInt(splits.ByComma(utils.Strip(input)))
+	min, max := utils.Min(crabs...), utils.Max(crabs...)
+
+	var output int = math.MaxInt
+
+	for start := min; start <= max; start++ {
+		var fuel int = 0
+
+		for _, crab := range crabs {
+			var diff = utils.Abs(crab - start)
+			fuel += diff
+		}
+
+		if fuel < output {
+			output = fuel
+		}
+	}
+	return output
+}
+
+func PartTwo(input string) int {
+	crabs := utils.StringToInt(splits.ByComma(utils.Strip(input)))
+	min, max := utils.Min(crabs...), utils.Max(crabs...)
+
+	var output int = math.MaxInt
+
+	for start := min; start <= max; start++ {
+		var fuel int = 0
+
+		for _, crab := range crabs {
+			var diff = utils.Abs(crab - start)
+			fuel += cumulative_sum(diff)
+		}
+
+		if fuel < output {
+			output = fuel
+		}
+	}
+	return output
+}
+
+func cumulative_sum(num int) int {
+	return (num + 1) * num / 2
+}
+
+func main() {
+	var input string = utils.LoadInput(2021, 07)
+
+	utils.Answer("Part One: %d", PartOne(input))
+	utils.Answer("Part Two: %d", PartTwo(input))
+}
