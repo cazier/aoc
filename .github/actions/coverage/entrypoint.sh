8a11959cc72f714410d47d7c78ef87b0ebafea4c
Brendan Cazier <admin@brendancazier.com>
2022-12-07 16:30:51 +0000

    Completed action with push

--- .github/actions/coverage/entrypoint.sh
+++ .github/actions/coverage/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# curl https://uploader.codecov.io/verification.gpg | gpg --no-default-keyring --keyring trustedkeys.gpg --import
+curl https://uploader.codecov.io/verification.gpg | gpg --no-default-keyring --keyring trustedkeys.gpg --import
 
-# cd /tmp
-# curl -Os https://uploader.codecov.io/latest/linux/codecov
-# curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
-# curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
-# gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
-# sha256sum -c codecov.SHA256SUM
+cd /tmp
+curl -Os https://uploader.codecov.io/latest/linux/codecov
+curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
+curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
+gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
+sha256sum -c codecov.SHA256SUM
 
-# chmod +x codecov
-# mv codecov /bin/codecov
+chmod +x codecov
 
-cd $GITHUB_WORKSPACE
-ls
-
-# ./codecov -t $1 -f coverage.xml
+if [ "$INPUT_DRY_RUN" == "true" ]; then
+    ./codecov -R $GITHUB_WORKSPACE -t $INPUT_SECRET -f $GITHUB_WORKSPACE/$INPUT_COVERAGE_FILE -d
+else
+    ./codecov -R $GITHUB_WORKSPACE -t $INPUT_SECRET -f $GITHUB_WORKSPACE/$INPUT_COVERAGE_FILE
+fi
