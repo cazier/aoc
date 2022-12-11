849414f90fc41ff3972ffe5ac4ca380a64d850a7
Brendan Cazier <admin@brendancazier.com>
2022-12-11 03:06:48 +0000

    Cleaner ordering for puzzle days only

--- python/utils/conftest.py
+++ python/utils/conftest.py
@@ -10,6 +10,14 @@ from ward.testing import Test
 
 @hook  # type: ignore
 def preprocess_tests(config: Config, collected_tests: list[Test]) -> None:  # pylint: disable=unused-argument
+    for test in collected_tests:
+        if test.module_name == "test_main":
+            # break if there is a module named `test_main` which is a year/day module in order to do sorting
+            break
+
+    else:
+        return
+
     collected_tests.sort(key=lambda k: k.description)
 
 
