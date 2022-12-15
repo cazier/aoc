841b2b96967b8c1308c56b88ee054adde2b43f52
Brendan Cazier <admin@brendancazier.com>
2022-12-15 22:23:23 +0000

    Swapped out Device type

--- python/year2022/day10/main.py
+++ python/year2022/day10/main.py
@@ -1,75 +1,13 @@
-import typing as t
 import pathlib
 import itertools
 
 from rich import print  # pylint: disable=redefined-builtin
+from year2022.day10.device import Device
 
 import utils
 from utils.display import Display
 
 SAMPLE_INPUT: str = pathlib.Path(__file__).parent.joinpath("sample.txt").read_text(encoding="utf8")
-
-
-class Device:  # pylint: disable=missing-class-docstring
-    cycle: int = 1
-    register: int = 1
-    commands: list[t.Iterator[int]] = []
-    screen: Display = Display(6, 40)
-
-    def __init__(self, commands: list[str]):
-        self.commands = []
-
-        for command, *value in (cmd.split() for cmd in reversed(commands)):
-            if command == "noop":
-                self.commands.append(self._noop(None))
-
-            elif command == "addx":
-                self.commands.append(self._addx(value[0]))
-
-    def run(self) -> int:
-        strength = 0
-
-        while True:
-            command = self.commands.pop()
-
-            for out in iter(command):
-                self.cycle += 1
-                self.register += out
-
-                if self.cycle in [20, 60, 100, 140, 180, 220]:
-                    strength += self.cycle * self.register
-
-                if self.cycle > 220:
-                    return strength
-
-    def draw(self) -> int:
-        while True:
-            command = self.commands.pop()
-
-            for out in iter(command):
-                self.color_pixel()
-                self.cycle += 1
-                self.register += out
-
-                if self.cycle > 240:
-                    return 1
-
-    def color_pixel(self) -> None:
-        cycle = self.cycle - 1
-
-        row = cycle // 40
-        column = cycle % 40
-
-        self.screen.draw(column, row, "#" if abs(self.register - column) <= 1 else ".")
-
-    @staticmethod
-    def _noop(_: None) -> t.Iterator[int]:
-        yield 0
-
-    @staticmethod
-    def _addx(command_value: str) -> t.Iterator[int]:
-        for value in [0, int(command_value)]:
-            yield value
 
 
 def part_one(inputs: str) -> int:
