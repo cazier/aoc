16f074820c530005463b9167aebcece42a412a1c
Brendan Cazier <520246+cazier@users.noreply.github.com>
2024-12-07 14:38:19 +0000

    New python based CLI

--- aoc/main.py
+++ aoc/main.py
@@ -1,20 +1,30 @@
+import enum
 import typing
+import pathlib
 import datetime
 import importlib
-from typing import Annotated
 
 import typer
+import pytest
 
 import aoclib
 
+
+class Action(enum.StrEnum):
+    RUN = "run"
+    TEST = "test"
+
+
 app = typer.Typer()
+python = typer.Typer()
+app.add_typer(python, name="python")
 
 
-@app.command()
+@python.command()
 def run(
     year: int = datetime.date.today().year,
     day: int = datetime.date.today().day,
-    part: Annotated[int, typer.Option(show_default="Both parts")] = -1,
+    part: typing.Annotated[int, typer.Option(show_default="Both parts")] = -1,
 ) -> None:
     module = importlib.import_module(f".year{year:04d}.day{day:02d}.main", package="aoc")
 
@@ -25,12 +35,19 @@ def run(
         print(module.part_two(aoclib.load_input(f"{year:04d}", f"{day:02d}")))
 
 
-@app.command()
+@python.command()
 def test(
-    year: int = datetime.date.today().year, day: int = datetime.date.today().day, part: typing.Optional[int] = None
+    year: int = datetime.date.today().year,
+    day: int = datetime.date.today().day,
 ) -> None:
-    module = importlib.import_module(f".year{year:04d}.day{day:02d}.main", package="aoc")
-    print(module.part_one("hello-world"))
+    path = pathlib.Path(__file__).parent.joinpath(f"year{year:04d}", f"day{day:02d}")
+
+    exit(pytest.main(["-v", str(path)]))
+
+
+@app.command()
+def new(year: int = datetime.date.today().year, day: int = datetime.date.today().day) -> None:
+    pass
 
 
 if __name__ == "__main__":
