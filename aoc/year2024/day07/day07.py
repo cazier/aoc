bda199f6ea949c0d12b02dee9f4bd810614ea0f5
Brendan Cazier <520246+cazier@users.noreply.github.com>
2024-12-08 16:07:32 -0500

    Leftover fixes (#15)
    * 2024-07
    
    * Moved some last files
    
    * Fixing go workflow problems

---
+++ aoc/year2024/day07/day07.py
@@ -0,0 +1,51 @@
+from rich import print  # pylint: disable=redefined-builtin
+
+import aoclib
+
+SAMPLE_INPUT: str = """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+"""
+
+
+def parse(inputs: str) -> list[tuple[int, list[int]]]:
+    def split(line: str) -> tuple[int, list[int]]:
+        result, *operands = line.split()
+        return int(result), list(map(int, operands))
+
+    return [split(line) for line in inputs.replace(":", "").splitlines() if line]
+
+
+def _eval(expected: int, a: int, *operands: int, pipe: bool = False) -> bool:
+    if not operands:
+        return a == expected
+
+    b, *bs = operands
+
+    return (
+        _eval(expected, a * b, *bs, pipe=pipe)
+        or _eval(expected, a + b, *bs, pipe=pipe)
+        or (pipe and _eval(expected, int(f"{a}{b}"), *bs, pipe=pipe))
+    )
+
+
+def part_one(inputs: str) -> int:
+    return sum(expected for expected, (a, *bs) in parse(inputs) if _eval(expected, a, *bs))
+
+
+def part_two(inputs: str) -> int:
+    return sum(expected for expected, (a, *bs) in parse(inputs) if _eval(expected, a, *bs, pipe=True))
+
+
+if __name__ == "__main__":  # pragma: no cover
+    input_string = aoclib.load_input()
+
+    print(part_one(input_string))
+    print(part_two(input_string))
